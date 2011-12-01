1935e5487bf680eef269b7bac1fc581b490d63ae
Nancy Ouyang <nouyang@mit.edu>
2011-12-01 01:27:28 -0500

    really terrible code but pov works

--- v0.1.45.c
+++ v0.1.45.c
@@ -12,18 +12,17 @@
 #define LINE_A 0
 #define LINE_B 1
 #define LINE_C 2
-#define IMGLEN 8
+#define IMGLEN 18
 
-#define B3(x)	((x&0b00000001)?0:0)  \
-                 +((x&0b00000010)?2:0)  \
+//~ #define B3(x)	((x&0b00000001)?0:0)  \
+                 //~ +((x&0b00000010)?2:0)  \
                  
-#define B5(x)   ((x&0b00000001)?0:0)  \
-                 +((x&0b00000010)?1:0)  \
-                 +((x&0b00000100)?2:0)  \
-                 +((x&0b00001000)?3:0)  \
-                 +((x&0b00010000)?4:0) \
-                 +((x&0b00010000)?5:0) \
-                 
+//~ #define B5(x)   ((x&0b00000001)?0:0)  \
+                 //~ +((x&0b00000010)?1:0)  \
+                 //~ +((x&0b00000100)?2:0)  \
+                 //~ +((x&0b00001000)?3:0)  \
+                 //~ +((x&0b00010000)?4:0) \
+                 //~ +((x&0b00010000)?5:0) \
 //DDRB direction config for each LED (1 = output) //i/o
 const char led_dir[6] = {
   ( 1<<LINE_B | 1<<LINE_A & ~(1<<LINE_C)), //LED 0
@@ -31,7 +30,7 @@ const char led_dir[6] = {
   ( 1<<LINE_C | 1<<LINE_B & ~(1<<LINE_A)), //LED 2
   ( 1<<LINE_B | 1<<LINE_C & ~(1<<LINE_A)), //LED 3
   ( 1<<LINE_A | 1<<LINE_C & ~(1<<LINE_B)), //LED 4
-  ( 1<<LINE_C | 1<<LINE_A & ~(1<<LINE_B)), //LED 5 
+  ( 1<<LINE_C | 1<<LINE_A & ~(1<<LINE_B)), //LED 5
 };
 
 //PORTB output config for each LED (1 = High, 0 = Low) //hi/lo
@@ -43,8 +42,35 @@ const char led_out[6] = {
   ( 1<<LINE_A & ~(1<<LINE_C)), //LED 4  
   ( 1<<LINE_C & ~(1<<LINE_A)), //LED 5
 };
+void light_led(char led_num) { //led_num [0,5]
+	DDRB = led_dir[led_num];
+	PORTB = led_out[led_num];
+	_delay_us(50);
+}; 
 
-                 
+const static char bit[5] = {
+	0b00001,
+	0b00010,
+	0b00100,
+	0b01000,
+	0b10000,
+};
+
+void check_led(char bar, int i){
+	if (bar & bit[i]) { //which led to light up?
+		light_led(i);
+	 }
+	 else {
+		 _delay_us(70);
+	 }
+};
+
+void B5(char x) {
+	for(int j=0; j < 6; j++){
+		check_led(x,j);
+	};	
+};
+               
 //~ const static uint8_t image[4] = {
 	//~ B3(0b00), 
 	//~ B3(0b10), 
@@ -52,22 +78,39 @@ const char led_out[6] = {
 	//~ B3(0b00),
 //~ };
 
-const static uint8_t image[IMGLEN] = {
-	B5(0b00000),
-	B5(0b01110),
-	B5(0b11111),
-	B5(0b10001),
-	B5(0b10001),
-	B5(0b11111),
-	B5(0b01110),
-	B5(0b00000),
+const static char image[IMGLEN] = {
+	0b00011,
+	0b00111,
+	0b01111,
+	0b11111,
+	0b11111,
+	0b01111,
+	0b00011,
+//~ 0b00000, 
+//~ 0b00000, 
+//~ 0b00000, 
+//~ 0b01110, 
+//~ 0b11111,  
+//~ 0b10001, 
+//~ 0b10001, 
+//~ 0b11111, 
+//~ 0b01110, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
+0b00000, 
 }; 
 
-void light_led(char led_num) { //led_num must be from 0 to 19
-	DDRB = led_dir[led_num];
-	PORTB = led_out[led_num];
-	_delay_us(50);
-}; 
+//~ void light_leds(char led_state) { //led_state is on/off 1 bit per LED	
+//~ }; 
+
 
 void leds_off() {
 	DDRB = 0;
@@ -77,15 +120,19 @@ void leds_off() {
 void flash_test() {
 	for(int x=0; x < 6; x++){
 		light_led(x);
+		_delay_ms(10);
 	}
 };
 
 int main(void) {
 	while (1) {
-		flash_test();
-		//~ for(int x = 0; x < IMGLEN; x++){
-			//~ light_led( image[x] );
-		//~ }
+		//~ flash_test();
+		//~ light_led(3);
+		for(int x = 0; x < IMGLEN; x++){
+			B5(image[x]);
+			leds_off();
+			_delay_us(500);
+		}
 	}
 	return 0;
 }
